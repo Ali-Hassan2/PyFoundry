1104e26c6a5c958f1c818834b68bc48239f5127b
Ali Hassan <alihassan26032004@gmail.com>
2025-11-14 22:22:25 +0500

    Update extensions.py

--- moodlm/app/extensions.py
+++ moodlm/app/extensions.py
@@ -3,3 +3,13 @@ from flask_migrate import Migrate
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("POSTGRESURL") 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
